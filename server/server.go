6fccfbed9faf46148fc22e4bd13ea73e25932a52
Denis Moiseev <isdenmois@gmail.com>
2017-02-26 21:15:34 +0000

    Add socket io server handler

--- server/server.go
+++ server/server.go
@@ -9,8 +9,10 @@ func Start(films []Film) {
 	// router := mux.NewRouter()
 	filmsHandler := FilmsHandler{Films: films}
   	fs := http.FileServer(http.Dir("static"))
+	socket := SocketIOServer{Films: films}
 	// filmsHandler.Register(router)
-	http.Handle("/", fs)
 	http.HandleFunc("/api/films", filmsHandler.Serve)
+	http.Handle("/socket.io/", socket.Subscribe())
+	http.Handle("/", fs)
 	http.ListenAndServe(":9090", nil)
 }
---
+++ server/socket.go
@@ -0,0 +1,63 @@
+package server
+
+import (
+    "log"
+    "github.com/googollee/go-socket.io"
+    "github.com/go-vgo/robotgo"
+    "encoding/json"
+)
+
+type SocketIOServer struct {
+    Handler      *socketio.Server
+	Films       []Film
+}
+
+type KeyboardMessage struct {
+    Key         string      `json:key`
+    Modifiers   []string    `json:modifiers`
+}
+
+func (s *SocketIOServer) Subscribe() *socketio.Server {
+    if (s.Handler != nil) {
+        return s.Handler;
+    }
+
+    handler, err := socketio.NewServer(nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    handler.On("connection", s.handleConnection)
+    handler.On("error", s.handleError)
+
+    s.Handler = handler
+    return s.Handler
+}
+
+func (s SocketIOServer) handleConnection(so socketio.Socket) {
+        log.Println("on connection")
+
+        so.On("films_get", func(msg string) {
+            log.Println("on films_get")
+            so.Emit("films_send", s.Films)
+        })
+
+        so.On("keyboard", s.handleKeyboard)
+
+        so.On("disconnection", func() {
+            log.Println("on disconnect")
+        })
+}
+
+func (s SocketIOServer) handleKeyboard(keyList string) {
+    log.Println("on keyboard: ", keyList)
+
+    var keys KeyboardMessage
+    json.Unmarshal([]byte(keyList), &keys)
+    log.Println("parsed: ", keys)
+    robotgo.KeyTap(keys.Key, keys.Modifiers)
+}
+
+func (SocketIOServer) handleError(so socketio.Socket, err error) {
+        log.Println("error:", err)
+}
